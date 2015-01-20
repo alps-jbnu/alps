19082924bf14b168cf3c808cc4d5f5b13a9e6b04
jaewon <sjw0687@gmail.com>
2015-01-20 09:24:09 +0900

    Add users table

--- alps/model.py
+++ alps/model.py
@@ -3,5 +3,6 @@ __all__ = 'import_all_modules',
 
 def import_all_modules():
     # Write down all modules of SQLAlchemy models HERE!
-    # Example: import alps.user
-    pass
+    # To prevent merge conflict, sort names by alphabetical order.
+    # To skip PEP8 check, append '# noqa' comment to each of lines.
+    import alps.user  # noqa
