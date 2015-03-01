97037e9d6bbb35788662f2120b148502cca73871
jaewon <sjw0687@gmail.com>
2015-03-02 00:48:05 +0900

    Make all validators for register user

--- alps/forms.py
+++ alps/forms.py
@@ -1,39 +1,130 @@
+import re
+
 from flask.ext.wtf import Form
 from wtforms import BooleanField, PasswordField, TextField
-from wtforms.validators import Regexp, Length
+from wtforms.validators import (Email, EqualTo, InputRequired, Length, Regexp,
+                                ValidationError)
 
 from alps.db import session
 from alps.user import User
 
-__all__ = ('id_length_msg', 'login_error_msg', 'id_char_msg',
-           'password_length_msg', 'SignInForm', 'SignUpForm')
+__all__ = ('id_len_msg', 'login_error_msg', 'id_char_msg',
+           'pw_len_msg', 'SignInForm', 'SignUpForm')
+
+
+class UnicodeLength(object):
+    def __init__(self, min, max, message=None):
+        self.min = min
+        self.max = max
+        if not message:
+            message = \
+                'Field must be between {} and {} characters long.' \
+                .format(min, max)
+        self.message = message
+
+    def __call__(self, form, field):
+        len = 0
+        for char in field.data:
+            if re.match('[a-zA-Z0-9 ]', char):  # is alphanumeric?
+                len += 1
+            else:  # assume other characters are unicode.
+                len += 2
+        if not (self.min <= len and len <= self.max):
+            raise ValidationError(self.message)
+
+
+class OptionalLength(object):
+    def __init__(self, min=-1, max=-1, message=None):
+        self.min = min
+        self.max = max
+        if not message:
+            message = \
+                'Field must be between {} and {} characters long.' \
+                .format(min, max)
+        self.message = message
+
+    def __call__(self, form, field):
+        if field.data:
+            l = len(field.data)
+            if l < self.min or self.max != -1 and l > self.max:
+                raise ValidationError(self.message)
+
 
 login_error_msg = (
     '등록되지 않은 아이디이거나, '
     '아이디 또는 비밀번호를 잘못 입력하셨습니다.'
 )
 
+required_field_msg = '필수 입력 항목입니다.'
+required_validator = InputRequired(message=required_field_msg)
+
+id_required_msg = '아이디를 입력해주세요.'
+id_required_validator = InputRequired(message=id_required_msg)
+
+pw_required_msg = '비밀번호를 입력해주세요.'
+pw_required_validator = InputRequired(message=pw_required_msg)
+
 id_char_msg = '아이디는 영문, 숫자, 일부 특수문자만 가능합니다.'
+
+# The validator only allows English, Hangul, and numeric characters
+# and some special characters: '_', '.' and '-'.
 id_char_validator = Regexp('^[a-zA-Z0-9_.\\-]*$',
                            message=id_char_msg)
 
-id_length_msg = '아이디의 길이는 4자 이상, 25자 이하여야 합니다.'
-id_length_validator = Length(min=4, max=25,
-                             message=id_length_msg)
+id_len_msg = '아이디의 길이는 4자 이상, 25자 이하여야 합니다.'
+id_len_validator = Length(min=4, max=25,
+                          message=id_len_msg)
 
-password_length_msg = '비밀번호의 길이는 8자 이상, 50자 이하여야 합니다.'
-password_length_validator = Length(min=8, max=50,
-                                   message=password_length_msg)
+pw_len_msg = '비밀번호의 길이는 8자 이상, 50자 이하여야 합니다.'
+pw_len_validator = Length(min=8, max=50,
+                          message=pw_len_msg)
+
+pw_confirm_msg = '비밀번호를 다시 확인해주세요.'
+pw_confirm_validator = EqualTo('password', message=pw_confirm_msg)
+
+nickname_char_msg = '닉네임은 영문, 한글, 숫자, 사이 공백만 가능합니다.'
+
+# The validator only allows English, Hangul, digit and whitespace characters.
+# But, it doesn't allow leading or trailing whitespace
+# and two or more whitespaces.
+nickname_char_validator = Regexp(
+    '(?!^[ ].*$)(?!^.*[ ]$)(?!^.*[ ]{2,}.*$)^[a-zA-Z0-9ㄱ-ㅣ가-힣 ]*$',
+    message=nickname_char_msg
+)
+
+nickname_len_msg = '닉네임은 한글 1~10자, 영문 대소문자 2~20자, 숫자를 사용할 수 있습니다. (혼용가능)'
+nickname_len_validator = UnicodeLength(min=2,
+                                       max=20,
+                                       message=nickname_len_msg)
+
+email_msg = '올바른 이메일을 입력해주세요.'
+email_validator = Email(message=email_msg)
+
+name_char_msg = '이름에는 한글만 포함할 수 있습니다.'
+name_char_validator = Regexp('^[ㄱ-ㅣ가-힣]*$',
+                             message=name_char_msg)
+
+name_len_msg = '이름은 2자 이상, 10자 이하여야 합니다.'
+name_len_validator = OptionalLength(min=2, max=10,
+                                    message=name_len_msg)
+
+student_number_char_msg = '학번은 숫자로만 구성됩니다.'
+student_number_char_validator = Regexp('^[0-9]*$',
+                                       message=student_number_char_msg)
+
+student_number_len_msg = '학번은 9글자여야 합니다.'
+student_number_len_validator = OptionalLength(min=9, max=9,
+                                              message=student_number_len_msg)
 
 
 class SignInForm(Form):
     username = TextField(label='아이디',
-                         validator=[id_char_validator, id_length_validator])
+                         validators=[id_required_validator,
+                                     id_char_validator,
+                                     id_len_validator])
     password = PasswordField(label='패스워드',
-                             validator=[password_length_validator])
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+                             validators=[pw_required_validator,
+                                         pw_len_validator])
 
     def validate(self):
         if not super().validate():
@@ -60,25 +151,28 @@ class SignInForm(Form):
 
 
 class SignUpForm(Form):
-    # TODO: more arguments required in the below fields!
-
-    username = TextField(label='아이디', )
-    nickname = TextField(label='닉네임')
-    email = TextField(label='이메일')
-    password = PasswordField(label='패스워드')
-    confirm_password = PasswordField(label='패스워드 재확인')
-    name = TextField(label='이름')
+    username = TextField(label='아이디',
+                         validators=[required_validator,
+                                     id_char_validator,
+                                     id_len_validator])
+    nickname = TextField(label='닉네임',
+                         validators=[required_validator,
+                                     nickname_char_validator,
+                                     nickname_len_validator])
+    email = TextField(label='이메일',
+                      validators=[required_validator,
+                                  email_validator])
+    password = PasswordField(label='패스워드',
+                             validators=[required_validator,
+                                         pw_len_validator])
+    confirm_password = PasswordField(label='패스워드 재확인',
+                                     validators=[required_validator,
+                                                 pw_confirm_validator])
+    name = TextField(label='이름',
+                     validators=[name_char_validator,
+                                 name_len_validator])
     jbnu_student = BooleanField(label='전북대학교 학생입니까?')
-    student_number = TextField(label='학번')
+    student_number = TextField(label='학번',
+                               validators=[student_number_char_validator,
+                                           student_number_len_validator])
     department = TextField(label='학부(과)')
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def validate(self):
-        if not super().validate():
-            return False
-
-        # TODO: validate me!
-
-        return True
--- tests/form_test.py
+++ tests/form_test.py
@@ -1,7 +1,7 @@
 from flask import url_for
 
-from alps.forms import (id_length_msg, login_error_msg, id_char_msg,
-                        password_length_msg)
+from alps.forms import (id_len_msg, login_error_msg, id_char_msg,
+                        pw_len_msg)
 
 
 def test_sign_in_form(fx_flask_client, fx_request_context,
@@ -24,43 +24,43 @@ def test_sign_in_form(fx_flask_client, fx_request_context,
     response = post_form('noman', 'iamayesman')
     assert login_error_msg in response.data.decode(encoding='utf-8')
     assert id_char_msg not in response.data.decode(encoding='utf-8')
-    assert id_length_msg not in response.data.decode(encoding='utf-8')
-    assert password_length_msg not in response.data.decode(encoding='utf-8')
+    assert id_len_msg not in response.data.decode(encoding='utf-8')
+    assert pw_len_msg not in response.data.decode(encoding='utf-8')
 
     # 비밀번호만 틀린 경우
     response = post_form('yesman', 'iamasuperman')
     assert login_error_msg in response.data.decode(encoding='utf-8')
     assert id_char_msg not in response.data.decode(encoding='utf-8')
-    assert id_length_msg not in response.data.decode(encoding='utf-8')
-    assert password_length_msg not in response.data.decode(encoding='utf-8')
+    assert id_len_msg not in response.data.decode(encoding='utf-8')
+    assert pw_len_msg not in response.data.decode(encoding='utf-8')
 
     # 아이디에 특수문자를 포함한 경우
     response = post_form('alps!@#$', 'user4password')
     assert login_error_msg in response.data.decode(encoding='utf-8')
     assert id_char_msg in response.data.decode(encoding='utf-8')
-    assert id_length_msg not in response.data.decode(encoding='utf-8')
-    assert password_length_msg not in response.data.decode(encoding='utf-8')
+    assert id_len_msg not in response.data.decode(encoding='utf-8')
+    assert pw_len_msg not in response.data.decode(encoding='utf-8')
 
     # 아이디에 공백를 포함한 경우
     response = post_form('alps user', 'user5password')
     assert login_error_msg in response.data.decode(encoding='utf-8')
     assert id_char_msg in response.data.decode(encoding='utf-8')
-    assert id_length_msg not in response.data.decode(encoding='utf-8')
-    assert password_length_msg not in response.data.decode(encoding='utf-8')
+    assert id_len_msg not in response.data.decode(encoding='utf-8')
+    assert pw_len_msg not in response.data.decode(encoding='utf-8')
 
     # 아이디가 길이가 알맞지 않은 경우
     response = post_form('alp', 'user6password')
     assert login_error_msg in response.data.decode(encoding='utf-8')
     assert id_char_msg not in response.data.decode(encoding='utf-8')
-    assert id_length_msg in response.data.decode(encoding='utf-8')
-    assert password_length_msg not in response.data.decode(encoding='utf-8')
+    assert id_len_msg in response.data.decode(encoding='utf-8')
+    assert pw_len_msg not in response.data.decode(encoding='utf-8')
 
     # 비밀번호가 길이가 알맞지 않은 경우
     response = post_form('alpsuser', 'user7')
     assert login_error_msg in response.data.decode(encoding='utf-8')
     assert id_char_msg not in response.data.decode(encoding='utf-8')
-    assert id_length_msg not in response.data.decode(encoding='utf-8')
-    assert password_length_msg in response.data.decode(encoding='utf-8')
+    assert id_len_msg not in response.data.decode(encoding='utf-8')
+    assert pw_len_msg in response.data.decode(encoding='utf-8')
 
     # 올바른 경우
     response = post_form('hihi', 'hellohello')
