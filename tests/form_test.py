9426cb72cf7f20690405e11f901b72b19fe73495
jaewon <sjw0687@gmail.com>
2015-03-02 01:05:38 +0900

    Split large unit test into smaller unit tests

--- tests/form_test.py
+++ tests/form_test.py
@@ -14,54 +14,64 @@ def post_login_form(fx_flask_client, username, password):
         return response
 
 
-def test_sign_in_form(fx_flask_client, fx_request_context,
+def test_proper_login(fx_flask_client, fx_request_context,
                       fx_session, fx_users):
-    # 올바른 경우
     response = post_login_form(fx_flask_client, 'yesman', 'iamayesman')
     assert login_error_msg not in response.data.decode(encoding='utf-8')
 
-    # 아이디만 틀린 경우
+    response = post_login_form(fx_flask_client, 'hihi', 'hellohello')
+    assert login_error_msg not in response.data.decode(encoding='utf-8')
+
+
+def test_login_with_only_wrong_id(fx_flask_client, fx_request_context,
+                                  fx_session, fx_users):
     response = post_login_form(fx_flask_client, 'noman', 'iamayesman')
     assert login_error_msg in response.data.decode(encoding='utf-8')
     assert id_char_msg not in response.data.decode(encoding='utf-8')
     assert id_len_msg not in response.data.decode(encoding='utf-8')
     assert pw_len_msg not in response.data.decode(encoding='utf-8')
 
-    # 비밀번호만 틀린 경우
+
+def test_login_with_only_wrong_pw(fx_flask_client, fx_request_context,
+                                  fx_session, fx_users):
     response = post_login_form(fx_flask_client, 'yesman', 'iamasuperman')
     assert login_error_msg in response.data.decode(encoding='utf-8')
     assert id_char_msg not in response.data.decode(encoding='utf-8')
     assert id_len_msg not in response.data.decode(encoding='utf-8')
     assert pw_len_msg not in response.data.decode(encoding='utf-8')
 
-    # 아이디에 특수문자를 포함한 경우
+
+def test_login_when_id_has_special_char(fx_flask_client, fx_request_context,
+                                        fx_session, fx_users):
     response = post_login_form(fx_flask_client, 'alps!@#$', 'user4password')
     assert login_error_msg in response.data.decode(encoding='utf-8')
     assert id_char_msg in response.data.decode(encoding='utf-8')
     assert id_len_msg not in response.data.decode(encoding='utf-8')
     assert pw_len_msg not in response.data.decode(encoding='utf-8')
 
-    # 아이디에 공백를 포함한 경우
+
+def test_login_when_id_has_space(fx_flask_client, fx_request_context,
+                                 fx_session, fx_users):
     response = post_login_form(fx_flask_client, 'alps user', 'user5password')
     assert login_error_msg in response.data.decode(encoding='utf-8')
     assert id_char_msg in response.data.decode(encoding='utf-8')
     assert id_len_msg not in response.data.decode(encoding='utf-8')
     assert pw_len_msg not in response.data.decode(encoding='utf-8')
 
-    # 아이디가 길이가 알맞지 않은 경우
+
+def test_login_with_improper_len_of_id(fx_flask_client, fx_request_context,
+                                       fx_session, fx_users):
     response = post_login_form(fx_flask_client, 'alp', 'user6password')
     assert login_error_msg in response.data.decode(encoding='utf-8')
     assert id_char_msg not in response.data.decode(encoding='utf-8')
     assert id_len_msg in response.data.decode(encoding='utf-8')
     assert pw_len_msg not in response.data.decode(encoding='utf-8')
 
-    # 비밀번호가 길이가 알맞지 않은 경우
+
+def test_login_with_improper_len_of_pw(fx_flask_client, fx_request_context,
+                                       fx_session, fx_users):
     response = post_login_form(fx_flask_client, 'alpsuser', 'user7')
     assert login_error_msg in response.data.decode(encoding='utf-8')
     assert id_char_msg not in response.data.decode(encoding='utf-8')
     assert id_len_msg not in response.data.decode(encoding='utf-8')
     assert pw_len_msg in response.data.decode(encoding='utf-8')
-
-    # 올바른 경우
-    response = post_login_form(fx_flask_client, 'hihi', 'hellohello')
-    assert login_error_msg not in response.data.decode(encoding='utf-8')
