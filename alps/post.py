b540bbab5c81012875512b745d236ed53c705c62
jaewon <sjw0687@gmail.com>
2015-03-07 23:44:35 +0900

    Add boards table

--- alps/post.py
+++ alps/post.py
@@ -4,9 +4,26 @@ from sqlalchemy.sql.functions import now
 from sqlalchemy.types import DateTime, Integer, String, Text
 
 from alps.db import Base
-from alps.user import User
+from alps.user import MemberType, User
 
 __all__ = 'Post',
+
+
+class Board(Base):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False, unique=True)
+    text = Column(String(100), nullable=False)
+    read_permission = Column(Integer, nullable=False,
+                             default=MemberType.non_member.value,
+                             server_default='0')
+    write_permission = Column(Integer, nullable=False,
+                              default=MemberType.non_member.value,
+                              server_default='0')
+
+    created_at = Column(DateTime(timezone=True), nullable=False,
+                        default=now())
+
+    __tablename__ = 'boards'
 
 
 class Post(Base):
@@ -16,6 +33,9 @@ class Post(Base):
 
     user_id = Column(Integer, ForeignKey(User.id))
     user = relationship(User)
+
+    board_id = Column(Integer, ForeignKey(Board.id))
+    board = relationship(Board)
 
     created_at = Column(DateTime(timezone=True), nullable=False,
                         default=now())
