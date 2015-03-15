8228af28a4b3400fce1c7d76266df3d0840512f5
jaewon <sjw0687@gmail.com>
2015-03-15 18:08:18 +0900

    Test board has posts, improve readability of tests

--- tests/post_test.py
+++ tests/post_test.py
@@ -4,9 +4,9 @@ from alps.post import Board, Post
 def test_posts_are_created(fx_session, fx_posts):
     assert fx_session.query(Post).count() > 0
     hello_post = fx_session.query(Post) \
-                           .filter_by(title='Hello, ALPS!') \
+                           .filter_by(title='이 곳은 임원게시판입니다.') \
                            .first()
-    assert hello_post == fx_posts.post_3
+    assert hello_post == fx_posts.executive_post_1
 
 
 def test_boards_are_created(fx_session, fx_boards):
@@ -14,4 +14,11 @@ def test_boards_are_created(fx_session, fx_boards):
     member_board = fx_session.query(Board) \
                              .filter_by(name='member') \
                              .first()
-    assert member_board == fx_boards.board_2
+    assert member_board == fx_boards.member_board
+
+
+def test_board_has_posts(fx_session, fx_boards, fx_posts):
+    free_posts = fx_session.query(Post) \
+                           .filter_by(board=fx_boards.free_board) \
+                           .all()
+    assert len(free_posts) == 2
