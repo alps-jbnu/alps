1d0d1f9d274c9afca5aef6d2ff334a44a5c895d2
jaewon <sjw0687@gmail.com>
2015-03-16 00:38:52 +0900

    Random post generation for dummy data!

--- alps/dummy.py
+++ alps/dummy.py
@@ -1,6 +1,9 @@
+import random
+
 from alps.db import session
+from alps.fixture import insert_initial_data
 from alps.model import import_all_modules
-from alps.user import MemberType, User
+from alps.user import User
 from alps.post import Board, Post
 
 __all__ = 'insert_dummy_data',
@@ -8,71 +11,36 @@ __all__ = 'insert_dummy_data',
 
 def insert_dummy_data(app):
     import_all_modules()
-
-    user_1 = User(
-        username='yesman',
-        nickname='그래',
-        email='yes@alps.jbnu.ac.kr',
-        name='장그래',
-        description='안녕하세요. 장그래입니다.',
-        email_validated=True,
-        member_type=MemberType.non_member.value,
-    )
-    user_1.set_password('iamayesman')
-
-    user_2 = User(
-        username='hihi',
-        nickname='안녕',
-        email='hi@alps.jbnu.ac.kr',
-        name='안영이',
-        email_validated=True,
-        member_type=MemberType.member.value,
-    )
-    user_2.set_password('hellohello')
-
-    user_3 = User(
-        username='alps2',
-        nickname='알프스2',
-        email='alps2@alps.jbnu.ac.kr',
-        name='알프스',
-        description='알프스입니다.',
-        is_jbnu_student=True,
-        student_number='101512345',
-        department='컴퓨터공학부',
-        email_validated=True,
-        member_type=MemberType.executive.value,
-    )
-    user_3.set_password('alpspassword')
-
-    free_board = Board(name='free2', text='자유게시판')
-    member_board = Board(name='member2', text='회원게시판',
-                         read_permission=MemberType.member.value,
-                         write_permission=MemberType.member.value)
-    executive_board = Board(name='executive2', text='임원게시판',
-                            read_permission=MemberType.executive.value,
-                            write_permission=MemberType.executive.value)
-
-    post_1 = Post(
-        title='첫 게시글입니다.',
-        content='안녕하세요. 테스트를 위한 글입니다.',
-        user=user_2,
-        board=free_board
-    )
-    post_2 = Post(
-        title='안녕 세상아!',
-        content='야호!',
-        user=user_1,
-        board=member_board
-    )
-    post_3 = Post(
-        title='Hello, ALPS!',
-        content='I just wanted to say hello',
-        user=user_3,
-        board=executive_board
-    )
+    insert_initial_data(app)
 
     with app.app_context():
-        with session.begin():
-            session.add_all([user_1, user_2, user_3])
-            session.add_all([free_board, member_board, executive_board])
-            session.add_all([post_1, post_2, post_3])
+        admin_user = session.query(User) \
+                            .filter_by(username='admin') \
+                            .first()
+        alps_user = session.query(User) \
+                           .filter_by(username='alps') \
+                           .first()
+        free_board = session.query(Board).filter_by(name='free').first()
+        member_board = session.query(Board).filter_by(name='member').first()
+        qna_board = session.query(Board).filter_by(name='qna').first()
+
+        users = (admin_user, alps_user)
+        sentences = (
+            '안녕하십니까?', '오랜만입니다.', '잘 지내시죠?', '처음 뵙겠습니다.',
+            '만나서 기쁩니다.', '만나 뵙게 되어 영광입니다.', '또 봅시다.',
+            '좋은 하루 되세요!', '행운을 빕니다.', '고맙습니다.', '실례합니다.',
+            '괜찮습니다.', '저 좀 도와주세요.', '행복하세요!', '안녕하세요.',
+            '테스트입니다.', '질문이 있습니다.', '여러분께 드릴 말씀이 있습니다.',
+            'Hello, World!', 'This is a test.', 'Have a nice day!',
+        )
+        boards = (free_board, member_board, qna_board)
+
+        for i in range(random.randrange(100, 201)):
+            random_post = Post(
+                title=random.choice(sentences),
+                content=random.choice(sentences),
+                user=random.choice(users),
+                board=random.choice(boards)
+            )
+            with session.begin():
+                session.add_all([random_post])
